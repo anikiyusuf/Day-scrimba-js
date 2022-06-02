d736e8c934bb77ba2fdd35ddda88bacfdf150b50
Aniki Yusuf <anikiyusuf130@gmail.com>
2022-06-02 13:13:11 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,90 @@
+//let count = 0;
+//
+//console.log(count);
+
+
+
+//let myAge =24
+
+//console.log(myAge);
+
+
+
+//let myAge = 24;
+//let humanDogYears = myAge * 7;
+//console.log(humanDogYears);
+
+
+//let count = 5;
+//count  =  count + 1;
+//console.log(count);
+/*
+let bonusPoints = 50
+console.log(bonusPoints);
+
+bonusPoints = bonusPoints + 50
+console.log(bonusPoints);
+
+bonusPoints =bonusPoints - 75
+console.log(bonusPoints);
+
+bonusPoints =bonusPoints + 45
+console.log(bonusPoints);*/
+/*
+let countEl = document.getElementById("count-el")//pass argument
+
+console.log(countEl)
+let count = 0
+function increment(){
+
+    count = count + 1
+    countEl.innerText = count
+    
+}*/
+/*
+let countEl = document.getElementById("count-el")
+
+console.log(countEl)
+
+let count = 0
+function increment(){
+    count = count + 1
+    countEl.innerText=count
+} 
+
+function decrement(){
+    count = count - 1
+    countEl.innerText = count
+}*/
+
+let countEl = document.getElementById("count-el")
+
+console.log(countEl)
+
+count = 0
+function increment(){
+    count = count + 1
+    countEl.innerText= count
+}
+function decrement(){
+    count = count - 1
+    countEl.innerText= count
+}
+function myAlert(){
+    alert("Welcome")
+}
+
+//function myLogger(){
+  //  console.log("Hello World");
+//}
+//myLogger()
+/*
+let lap1 =34
+let lap2 =33
+let lap3 =32
+
+function lopLapTime(){
+    let lap1 =34
+    let lap2 =33
+    let lap3 =32
+    console.log(lap1 + lap2 + lap3*/
